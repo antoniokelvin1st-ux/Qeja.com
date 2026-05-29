f2d556898b7750a33c54e5f97fe6290e01935a87
antoniokelvin1st-ux <antoniokelvin1st@gmail.com>
2026-05-29 14:00:39 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,19 @@
-// Firebase SDK imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
 
-// Firebase config
 const firebaseConfig = {
     apiKey: "AIzaSyDp33VJKZSPDYwU5OY_Nzq-fnAck50-bDA",
     authDomain: "qeja-com.firebaseapp.com",
+    databaseURL: "https://qeja-com-default-rtdb.firebaseio.com",
     projectId: "qeja-com",
     storageBucket: "qeja-com.firebasestorage.app",
     messagingSenderId: "219739142262",
     appId: "1:219739142262:web:52efbfb97bb07c32a94757"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Firebase services
 export const auth = getAuth(app);
 export const database = getDatabase(app);
 
